2d09937e4fc142fd8462ffd9b7dda9bb2a0aa684
abrarZhr <Abrar.alzhrani.ruh.js@tuwaiq.edu.sa>
2021-11-07 22:56:18 +0300

    lab Done

---
+++ task1/student.js
@@ -0,0 +1,7 @@
+module.exports ={
+    // key + value 
+
+    printName : (NameS) =>{
+        console.log("stedent is "+NameS)
+    }
+}
---
+++ task2/shape.js
@@ -0,0 +1,4 @@
+let width =0;
+let height =0;
+
+module.exports = {width , height}
